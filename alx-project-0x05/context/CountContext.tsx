65abb233da97382fdeea3044667a7cceacd6ee1d
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-26 15:33:24 +0100

    Update CountContext.tsx

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -8,12 +8,11 @@ interface CountContextProps {
 
 export const CountContext = createContext<CountContextProps | undefined>(undefined)
 
-export const CountProvider = ({ children }: { children: ReactNode}) => {
-
+export const CountProvider = ({ children }: { children: ReactNode }) => {
   const [count, setCount] = useState<number>(0)
 
-  const increment = () => setCount((count ) =>count + 1)
-  const decrement = () => setCount((count) => count > 0 ? count - 1 : 0)
+  const increment = () => setCount((count) => count + 1)
+  const decrement = () => setCount((count) => (count > 0 ? count - 1 : 0))
 
   return (
     <CountContext.Provider value={{ count, increment, decrement }}>
@@ -26,7 +25,7 @@ export const useCount = () => {
   const context = useContext(CountContext)
 
   if (!context) {
-    throw new Error("useCount must be within a Count Provider")
+    throw new Error("useCount must be within a CountProvider")
   }
 
   return context
